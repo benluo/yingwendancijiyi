59ff8e1dff9c9d24ac4e9a97440b1724f6f7ac70
Ben Luo <benluo@gmail.com>
2018-07-09 01:30:02 +0800

    not work perfect

--- tools/dictation.py
+++ tools/dictation.py
@@ -2,10 +2,20 @@ import os
 import sqlite3
 import time
 import string
+import sys
 
 conn = sqlite3.connect('../words.db')
 c = conn.cursor()
 repeatTime = 2
+
+choice = input("Dictation in Order(input O) or in Random(input R)?")
+if choice in ['o','O']:
+    choice = input("Please select ")
+elif choice in ['r','R']:
+    choice = input("Please input how many words you want to dictation. Please input one number in range of 1 to 627.")
+else:
+    print ("Not right choice. Bye.")
+    sys.exit()
 
 for letter in string.ascii_lowercase:
     select = "SELECT id, voice,enword FROM words where enword like \"{}%\" order by id".format(letter)
@@ -16,10 +26,11 @@ for letter in string.ascii_lowercase:
         if not os.path.exists(filePath):
             print(voice)
         else:
-            for i in range(repeatTime):
-                os.system("mpg321 "+filePath)
-                time.sleep(3)
-
+            print(item[2])
+#  for i in range(repeatTime):
+#      os.system("mpg321 "+filePath)
+#      time.sleep(3)
+#
     nextLetter = input("Next letter? y(for yes) or n(for no)")
     if nextLetter == 'n':
         break
